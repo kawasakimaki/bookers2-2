7059f95380618ce322fa5cb49395190b16b3ace2
maki <kchurip.08@gmail.com>
2023-12-13 16:20:44 +0000

    ユーザー編集画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
 end
